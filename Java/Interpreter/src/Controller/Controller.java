46c7b41a6dc83fa23a411e4bed59e36592662b0e
Adrian Condrea <adriancondrea2000@gmail.com>
2020-11-01 21:44:09 +0200

    update

--- Java/Interpreter/src/Controller/Controller.java
+++ Java/Interpreter/src/Controller/Controller.java
@@ -1,18 +1,12 @@
 package Controller;
 
-import CustomException.*;
+import CustomException.CustomException;
 import Model.AbstractDataTypes.MyDictionary;
 import Model.AbstractDataTypes.MyList;
 import Model.AbstractDataTypes.MyStack;
 import Model.AbstractDataTypes.StackInterface;
-import Model.Expression.*;
 import Model.ProgramState;
-import Model.Statement.*;
-import Model.Type.BoolType;
-import Model.Type.IntegerType;
-import Model.Value.BooleanValue;
-import Model.Value.NumberValue;
-import Model.Value.Value;
+import Model.Statement.Statement;
 import Repository.Repository;
 
 
@@ -44,51 +38,12 @@ public class Controller {
         }
     }
 
-    public void newProgram(int program){
+    public void createState(Statement program){
+        //create a new state from the program given as parameter
         ProgramState state;
-        StackInterface<Statement> stack = new MyStack<Statement>();
-        Statement ex1 = new CompoundStatement(new VariableDeclarationStatement("v",new IntegerType()),
-                        new CompoundStatement(new AssignmentStatement("v",new ValueExpression(new NumberValue(2))),
-                              new PrintStatement(new VariableNameExpression("v"))));
-        Statement ex2 = new CompoundStatement( new VariableDeclarationStatement("a",new IntegerType()),
-                            new CompoundStatement(new VariableDeclarationStatement("b",new IntegerType()),
-                                    new CompoundStatement(new AssignmentStatement("a", new ArithmeticExpression('+',new ValueExpression(new NumberValue(2)),
-                                            new ArithmeticExpression('*',new ValueExpression(new NumberValue(3)), new ValueExpression(new NumberValue(5))))),
-                                                new CompoundStatement(new AssignmentStatement("b",new ArithmeticExpression('+',new VariableNameExpression("a"),
-                                                        new ValueExpression(new NumberValue(1)))), new PrintStatement(new VariableNameExpression("b"))))));
-        Statement ex3 = new CompoundStatement(new VariableDeclarationStatement("a",new BoolType()),
-                            new CompoundStatement(new VariableDeclarationStatement("v", new IntegerType()),
-                                new CompoundStatement(new AssignmentStatement("a", new ValueExpression(new BooleanValue(true))),
-                                    new CompoundStatement(new ConditionalStatement(new VariableNameExpression("a"),new AssignmentStatement("v",
-                                            new ValueExpression(new NumberValue(2))), new AssignmentStatement("v",
-                                                new ValueExpression(new NumberValue(3)))), new PrintStatement(new VariableNameExpression("v"))))));
-
-        Statement ex4 = new CompoundStatement(new VariableDeclarationStatement("a", new IntegerType()), new AssignmentStatement("a", new ValueExpression(new NumberValue(5))));
-
-        Statement ex5 = new CompoundStatement(new VariableDeclarationStatement("a", new BoolType()), new AssignmentStatement("a", new ValueExpression(new NumberValue(5))));
-
-        Statement ex6 = new CompoundStatement(
-                new CompoundStatement(new VariableDeclarationStatement("a", new BoolType()), new AssignmentStatement("a", new ValueExpression(new BooleanValue(false)))),
-                new PrintStatement(new LogicExpression('&', new VariableNameExpression("a"), new ValueExpression(new BooleanValue(true)))));
-        /*
-        Statement ex7 = new CompoundStatement(new CompoundStatement(new VariableDeclarationStatement("a", new BoolType()),
-                            new VariableDeclarationStatement("b", new BoolType())), new CompoundStatement(
-                                new CompoundStatement(new AssignmentStatement("a", new ValueExpression(new BooleanValue(true))),
-                                        new AssignmentStatement("b", new ValueExpression(new BooleanValue(false))))),
-                        new CompoundStatement(new PrintStatement(new LogicExpression('&', new VariableNameExpression("a"), new VariableNameExpression("b"))),
-                                new PrintStatement(new LogicExpression('|', new VariableNameExpression("a"), new VariableNameExpression("b")))));
-        */
-
-        switch (program) {
-            case 1 -> stack.push(ex1);
-            case 2 -> stack.push(ex2);
-            case 3 -> stack.push(ex3);
-            case 4 -> stack.push(ex4);
-            case 5 -> stack.push(ex5);
-            case 6 -> stack.push(ex6);
-        }
-
-        state = new ProgramState(stack, new MyDictionary<String, Value>(), new MyList<Value>(), null);
+        StackInterface<Statement> stack = new MyStack<>();
+        stack.push(program);
+        state = new ProgramState(stack, new MyDictionary<>(), new MyList<>(), null);
         repository.addState(state);
     }
 }
--- Java/Interpreter/src/View/View.java
+++ Java/Interpreter/src/View/View.java
@@ -1,15 +1,53 @@
 package View;
 
 import Controller.Controller;
+import Model.Expression.ArithmeticExpression;
+import Model.Expression.LogicExpression;
+import Model.Expression.ValueExpression;
+import Model.Expression.VariableNameExpression;
+import Model.Statement.*;
+import Model.Type.BoolType;
+import Model.Type.IntegerType;
+import Model.Value.BooleanValue;
+import Model.Value.NumberValue;
 import Repository.Repository;
 import Repository.memoryRepository;
 
 import java.util.Scanner;
 
+
 public class View {
+
+
     Controller controller;
 
-    public void printMenu() {
+    public void hardcodedMenu() {
+        //hardcoded menu options for first assignment
+        //prints options and lets the user choose which program to load
+        Statement ex1 = new CompoundStatement(new VariableDeclarationStatement("v",new IntegerType()),
+                new CompoundStatement(new AssignmentStatement("v",new ValueExpression(new NumberValue(2))),
+                        new PrintStatement(new VariableNameExpression("v"))));
+        Statement ex2 = new CompoundStatement( new VariableDeclarationStatement("a",new IntegerType()),
+                new CompoundStatement(new VariableDeclarationStatement("b",new IntegerType()),
+                        new CompoundStatement(new AssignmentStatement("a", new ArithmeticExpression('+',new ValueExpression(new NumberValue(2)),
+                                new ArithmeticExpression('*',new ValueExpression(new NumberValue(3)), new ValueExpression(new NumberValue(5))))),
+                                new CompoundStatement(new AssignmentStatement("b",new ArithmeticExpression('+',new VariableNameExpression("a"),
+                                        new ValueExpression(new NumberValue(1)))), new PrintStatement(new VariableNameExpression("b"))))));
+        Statement ex3 = new CompoundStatement(new VariableDeclarationStatement("a",new BoolType()),
+                new CompoundStatement(new VariableDeclarationStatement("v", new IntegerType()),
+                        new CompoundStatement(new AssignmentStatement("a", new ValueExpression(new BooleanValue(true))),
+                                new CompoundStatement(new ConditionalStatement(new VariableNameExpression("a"),new AssignmentStatement("v",
+                                        new ValueExpression(new NumberValue(2))), new AssignmentStatement("v",
+                                        new ValueExpression(new NumberValue(3)))), new PrintStatement(new VariableNameExpression("v"))))));
+
+        Statement ex4 = new CompoundStatement(new VariableDeclarationStatement("a", new IntegerType()), new AssignmentStatement("a", new ValueExpression(new NumberValue(5))));
+
+        Statement ex5 = new CompoundStatement(new VariableDeclarationStatement("a", new BoolType()), new AssignmentStatement("a", new ValueExpression(new NumberValue(5))));
+
+        Statement ex6 = new CompoundStatement(
+                new CompoundStatement(new VariableDeclarationStatement("a", new BoolType()), new AssignmentStatement("a", new ValueExpression(new BooleanValue(false)))),
+                new PrintStatement(new LogicExpression('&', new VariableNameExpression("a"), new ValueExpression(new BooleanValue(true)))));
+
         System.out.println("ex1:");
         System.out.println("int v;\n v=2;\nPrint(v)\n");
 
@@ -32,14 +70,21 @@ public class View {
 
         Scanner console = new Scanner(System.in);
         int choice = console.nextInt();
-        controller.newProgram(choice);
+        switch (choice) {
+            case 1 -> controller.createState(ex1);
+            case 2 -> controller.createState(ex2);
+            case 3 -> controller.createState(ex3);
+            case 4 -> controller.createState(ex4);
+            case 5 -> controller.createState(ex5);
+            case 6 -> controller.createState(ex6);
+        }
     }
 
     public View() {
         Repository repository = new memoryRepository();
         this.controller = new Controller(repository);
         controller.setDisplayState(true);
-        printMenu();
+        hardcodedMenu();
         try {
             controller.allStepExecution();
         } catch (Exception exception) {
